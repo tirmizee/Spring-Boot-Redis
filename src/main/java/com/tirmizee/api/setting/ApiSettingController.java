c4bd7392e58638c2ecc5d1a11b88adcee20d669f
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-01-12 16:57:15 +0700

    add jedis

--- src/main/java/com/tirmizee/api/setting/ApiSettingController.java
+++ src/main/java/com/tirmizee/api/setting/ApiSettingController.java
@@ -2,19 +2,24 @@ package com.tirmizee.api.setting;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import redis.clients.jedis.JedisPool;
 
 @RestController
 
 public class ApiSettingController {
 	
 	@Autowired
+	public JedisPool jedisPool;
+	
+	@Autowired
 	public RedisTemplate<String, Object> redisTemplate;
 	
 	@GetMapping(path = "/")
-	public String ssss() {
+	public String setting() {
+		System.out.println(jedisPool.getNumActive());
 		return "";
 	}
 
--- src/main/java/com/tirmizee/config/AppConfig.java
+++ src/main/java/com/tirmizee/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.tirmizee.core.config;
+package com.tirmizee.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,9 +6,17 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 @Configuration
 public class AppConfig {
 
+	@Bean
+	public JedisPool jedisPool() {
+		return new JedisPool(new JedisPoolConfig(),"localhost", 6379);
+	}
+	
 	@Bean
 	public JedisConnectionFactory jedisConnectionFactory() {
 		return new JedisConnectionFactory();
